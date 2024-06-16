19db22df139394e53365466d98fa260dd3a38868
Suren77777 <suren.saroyan03@mail.ru>
2024-06-16 05:41:39 +0400

    add constructor

--- TicTacToe/Models/MakeMove.cs
+++ TicTacToe/Models/MakeMove.cs
@@ -5,5 +5,12 @@ namespace TicTacToe.Models
         public int Row { get; set; }
         public int Col { get; set; }
         public char Player { get; set; }
+
+        public MakeMove(int x, int y, char player)
+        {
+            Row = x;
+            Col = y;
+            Player = player;
+        }
     }
 }
